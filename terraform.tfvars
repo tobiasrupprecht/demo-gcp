bf2a4691e4370ea6e059af13ae96ac1763750045
tobiasrupprecht <trupprecht@hashicorp.com>
2021-12-13 15:13:55 +0100

    First commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project = "terraform-learn-gcp-314009"
+credentials_file = "terraform-learn-gcp-314009-e689bd3dd0a8.json"
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project" { 
+    type = string
+}
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
